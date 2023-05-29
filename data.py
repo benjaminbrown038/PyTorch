d4e66e6a63330eb7327ed6b16f84e6672ea16e54
Benjamin Brown <35114142+benjaminbrown038@users.noreply.github.com>
2023-05-29 17:42:52 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,22 @@
-import torchvision
+from torch.utils.data import DataLoader
 
-import torch
+mnist_train = torchvision.datasets.MNIST(root = '/data', download = True,train = True,transform = transforms.ToTensor())
+mnist_test = torchvision.datasets.MNIST(root = '/data', download = True, train = False, transform = transforms.ToTensor())
 
-import numpy as np
+mnist_train_dataloader = DataLoader(mnist_train,batch_size = 64,shuffle = True)
+mnist_test_dataloader = DataLoader(mnist_test,batch_size = 64, shuffle = True)
+
+
+mnist_train1 = torchvision.datasets.MNIST(root = '/data', download = True,train = True,transform = transforms.Compose([transforms.ToTensor(),transforms.Grayscale(3)]))
+mnist_test1 = torchvision.datasets.MNIST(root = '/data', download = True, train = False, transform = transforms.Compose([transforms.ToTensor(),transforms.Grayscale(3)]))
+
+mnist_train_dataloader1 = DataLoader(mnist_train,batch_size = 64,shuffle = True)
+mnist_test_dataloader1 = DataLoader(mnist_test,batch_size = 64, shuffle = True)
+
+
+cifar_train = torchvision.datasets.MNIST(root = '/data', download = True, train = True)
+cifar_test = torchvision.datasets.MNIST(root = '/data',download = True, train = False)
+
+
+cifar_train_dataloader = DataLoader(mnist_train, batch_size = 64, shuffle = True)
+cifar_test_dataloader = DataLoader(mnist_test, batch_size = 64, shuffle = True)
