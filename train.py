0d1766c4860d18fef3922ea52247c47f0f11b6c0
Benjamin Brown <35114142+benjaminbrown038@users.noreply.github.com>
2023-05-29 17:46:41 -0600

    Update train.py

--- train.py
+++ train.py
@@ -1,19 +1,6 @@
-import torch
-from torch.nn import Sequential, Conv2d, MaxPool2d, Flatten, Linear, Softmax, CrossEntropyLoss
-
-
-model = nn.Sequential(
-			nn.Conv2d(1,32,kernel_size = 3),
-			nn.Conv2d(32,64,kernel_szie = 3),
-			nn.MaxPool2d(2),
-			nn.Dropout(0.25),
-			nn.Flatten(),
-			nn.Linear(9216,128),
-			nn.Linear(128,10),
-			nn.Softmax())
-
 mnist_train = torchvision.datasets.MNIST(root = '/data', download = True, train = True, transform = transforms.ToTensor())
 mnist_train_dataloader = DataLoader(mnist_train,batch_size = 64, shuffle = True)
+
 loss_fn = CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(),lr = .001, momentum = .9)
 
@@ -28,5 +15,4 @@ def train_loop(dataloader,model,loss_fn,optimizer):
         if batch % 100 == 0:
             loss,current = loss.item(),batch*len(x)
 
-PATH = '/path'
-torch.save('model.pkl',PATH)
+torch.save('model.pkl'))
