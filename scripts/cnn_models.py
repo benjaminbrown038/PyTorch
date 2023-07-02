a5435e6bce3eb7f141d054e40ea4903fbcf48588
Benjamin Brown <35114142+benjaminbrown038@users.noreply.github.com>
2023-07-01 18:33:19 -0600

    Update cnn_models.py

--- scripts/cnn_models.py
+++ scripts/cnn_models.py
@@ -1,3 +1,19 @@
+import torch
+from torch.models import Sequential
+from torch.layers import Conv2d, MaxPool2d, Dropout, Flatten, Linear, Softmax
+from torchsummary import summary
+from torchvision.models import resnet50, inception_v3, alexnet
+
+'''
+
+Sequential Model
+
+Need to add documentation behind logic of layers in conv model.
+
+
+'''
+
+
 model = nn.Sequential(
 			nn.Conv2d(1,32,kernel_size = 3),
 			nn.Conv2d(32,64,kernel_szie = 3),
@@ -8,7 +24,23 @@ model = nn.Sequential(
 			nn.Linear(128,10),
 			nn.Softmax()
 			)
+
+'''
+
+Return Output 
+
+'''
 summary(model,1,28,28)
+
+
+'''
+
+Sequential Model
+
+Need to add documentation behind logic of layers in conv model.
+
+
+'''
 
 model1 = nn.Sequential(
 		nn.Conv2d(1,32,kernel_size = 2),
@@ -22,8 +54,24 @@ model1 = nn.Sequential(
 		nn.Flatten(),
 		nn.Linear(90,10)
 		)
+
+
+'''
+
+Return Output
+
+'''
 summary(model,(1,28,28))
 
+
+'''
+
+Sequential Model
+
+Need to add documentation behind logic of layers in conv model.
+
+
+'''
 
 
 mdoel2 = nn.Sequential(
@@ -35,12 +83,50 @@ mdoel2 = nn.Sequential(
 		nn.Linear(9216,128),
 		nn.Linear(128,10)
 		)
+
+'''
+
+Return Output
+
+'''
+
 summary(model2,(1,28,28))
+
+
+'''
+
+Pretrained Model
+
+Need to add documentation. 
+Need to output summary of model.
+
+
+'''
 
 pre_trained_one = resnet50(pretrained = True)
 
 
+'''
+
+Pretrained Model
+
+Need to add documentation.
+Need to output summary of model.
+
+'''
+
 pre_trained_two = inception_v3(pretrained=True)
+
+
+'''
+
+Pretrained Model
+
+Need to add documentation. 
+Need to output summary of model.
+
+
+'''
 
 
 pre_trained_three = alexnet(pretrained = True)
